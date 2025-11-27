3d4e29971e217b856df91b860af6a71dbfa6daac
chdean-09 <chadandrada20@gmail.com>
2025-11-28 03:32:22 +0800

    fix errors

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { FeedGateway } from './feed.gateway';
 import { FeedController } from './feed.controller';
 import { FeedService } from './feed.service';
+import { PrismaModule } from 'src/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [FeedGateway, FeedService],
   controllers: [FeedController],
 })
